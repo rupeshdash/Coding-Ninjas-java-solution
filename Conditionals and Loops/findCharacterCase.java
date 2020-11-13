fdce561652fe7910ed6169ed8cb6efcfd7cf5fe5
Rupesh Kumar Dash <60392205+rupeshdash@users.noreply.github.com>
2020-11-13 22:02:13 +0530

    Create findCharacterCase.java

---
+++ Conditionals and Loops/findCharacterCase.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner s=  new Scanner(System.in);
+            String str= s.nextLine();
+        	char c=str.charAt(0);
+        	int i=c;
+            
+            if(i>=65 && i<=90)
+{
+	System.out.println("1");
+}
+
+else if(i>=97 && i<=122)
+{
+	System.out.println("0");
+}
+
+else
+{
+	System.out.println("-1");
+}
+         
+    }
+}
